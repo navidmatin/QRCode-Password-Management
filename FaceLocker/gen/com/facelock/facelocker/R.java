04ba397b6781689604abb521e58a8ae4d4819eaf
navidmatin <seyedmatin.1@osu.edu>
2014-03-05 19:10:34 -0500

    Added Register activity and layout -Still not functional

--- FaceLocker/gen/com/facelock/facelocker/R.java
+++ FaceLocker/gen/com/facelock/facelocker/R.java
@@ -25,33 +25,42 @@ public final class R {
     }
     public static final class id {
         public static final int TextView01=0x7f080002;
-        public static final int action_forgot_password=0x7f08001a;
-        public static final int action_settings=0x7f08001b;
-        public static final int appInput=0x7f080016;
+        public static final int action_forgot_password=0x7f080023;
+        public static final int action_settings=0x7f080024;
+        public static final int appInput=0x7f08001f;
         public static final int autoCompleteTextView1=0x7f080003;
-        public static final int btnLogin=0x7f080019;
+        public static final int btnLogin=0x7f080022;
         public static final int button1=0x7f080008;
+        public static final int confirm_password=0x7f08001b;
         public static final int container=0x7f080000;
+        public static final int create_new_user_btn=0x7f08001c;
         public static final int editText1=0x7f080005;
         public static final int editText2=0x7f080007;
-        public static final int email=0x7f08000f;
-        public static final int footer=0x7f080015;
-        public static final int header=0x7f080014;
+        public static final int footer=0x7f08001e;
+        public static final int header=0x7f08001d;
         public static final int lblListHeader=0x7f08000a;
         public static final int lblListItem=0x7f08000b;
         public static final int login=0x7f080011;
         public static final int login_form=0x7f08000e;
         public static final int login_status=0x7f08000c;
         public static final int login_status_message=0x7f08000d;
-        public static final int passInput=0x7f080018;
+        public static final int passInput=0x7f080021;
         public static final int password=0x7f080010;
-        public static final int pwGroupList=0x7f080013;
+        public static final int pwGroupList=0x7f080014;
+        public static final int register=0x7f08001a;
+        public static final int register_button=0x7f080013;
+        public static final int register_form=0x7f080017;
+        public static final int register_password=0x7f080019;
+        public static final int register_status=0x7f080015;
+        public static final int register_status_message=0x7f080016;
+        public static final int register_username=0x7f080018;
         public static final int section_label=0x7f080009;
         public static final int sign_in_button=0x7f080012;
         public static final int textView1=0x7f080001;
         public static final int textView3=0x7f080004;
         public static final int textView4=0x7f080006;
-        public static final int userInput=0x7f080017;
+        public static final int userInput=0x7f080020;
+        public static final int username=0x7f08000f;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -63,36 +72,41 @@ public final class R {
         public static final int list_item=0x7f030006;
         public static final int login=0x7f030007;
         public static final int my_passwords=0x7f030008;
-        public static final int test=0x7f030009;
+        public static final int register=0x7f030009;
+        public static final int test=0x7f03000a;
     }
     public static final class menu {
         public static final int login=0x7f070000;
         public static final int main=0x7f070001;
     }
     public static final class string {
-        public static final int Application=0x7f050008;
-        public static final int Login=0x7f050009;
-        public static final int action_forgot_password=0x7f05000f;
+        public static final int Application=0x7f050007;
+        public static final int Login=0x7f050008;
+        public static final int action_forgot_password=0x7f050012;
+        public static final int action_register=0x7f050010;
         public static final int action_settings=0x7f050001;
-        public static final int action_sign_in_register=0x7f05000d;
-        public static final int action_sign_in_short=0x7f05000e;
-        public static final int add=0x7f050007;
+        public static final int action_sign_in=0x7f05000f;
+        public static final int action_sign_in_short=0x7f050011;
+        public static final int add=0x7f050006;
         public static final int app_name=0x7f050000;
-        public static final int error_field_required=0x7f050014;
-        public static final int error_incorrect_password=0x7f050013;
-        public static final int error_invalid_email=0x7f050011;
-        public static final int error_invalid_password=0x7f050012;
-        public static final int login_progress_signing_in=0x7f050010;
-        public static final int password=0x7f050006;
+        public static final int confirm_password=0x7f05000e;
+        public static final int error_field_required=0x7f050018;
+        public static final int error_incorrect_password=0x7f050017;
+        public static final int error_invalid_email=0x7f050015;
+        public static final int error_invalid_password=0x7f050016;
+        public static final int login_progress_signing_in=0x7f050013;
+        public static final int password=0x7f050005;
+        public static final int prompt_password=0x7f05000d;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f05000b;
-        public static final int prompt_password=0x7f05000c;
+        public static final int prompt_username=0x7f05000c;
+        public static final int register=0x7f050009;
+        public static final int register_progress_creating_new_user=0x7f050014;
         public static final int title_activity_login=0x7f05000a;
+        public static final int title_activity_register=0x7f05000b;
         public static final int title_section1=0x7f050002;
         public static final int title_section2=0x7f050003;
-        public static final int title_section3=0x7f050004;
-        public static final int username=0x7f050005;
+        public static final int username=0x7f050004;
     }
     public static final class style {
         /** 
--- FaceLocker/src/com/facelock/facelocker/Register.java
+++ FaceLocker/src/com/facelock/facelocker/Register.java
@@ -1,31 +1,24 @@
 package com.facelock.facelocker;
 
-
-import java.util.List;
-
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.KeyEvent;
-import android.view.Menu;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 /**
- * Activity which displays a login screen to the user, offering registration as
+ * Activity which displays a register screen to the user, offering registration as
  * well.
  */
-public class Login extends Activity {
+public class Register extends Activity {
 	/**
 	 * A dummy authentication store containing known user names and passwords.
 	 * TODO: remove after connecting to a real authentication system.
@@ -37,84 +30,78 @@ public class Login extends Activity {
 	 * The default email to populate the email field with.
 	 */
 	public static final String EXTRA_EMAIL = "com.example.android.authenticatordemo.extra.EMAIL";
-	
-	/**
-	 * Keep track of the login task to ensure we can cancel it if requested.
-	 */
-	private DatabaseHelper dh;
-	private UserLoginTask mAuthTask = null;
 
-	// Values for email and password at the time of the login attempt.
+	/**
+	 * Keep track of the register task to ensure we can cancel it if requested.
+	 */
+	private UserregisterTask mAuthTask = null;
+
+	// Values for email and password at the time of the register attempt.
 	private String mUsername;
 	private String mPassword;
 
 	// UI references.
 	private EditText mUsernameView;
 	private EditText mPasswordView;
-	private View mLoginFormView;
-	private View mLoginStatusView;
-	private TextView mLoginStatusMessageView;
+	private View mregisterFormView;
+	private View mregisterStatusView;
+	private TextView mregisterStatusMessageView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		setContentView(R.layout.login);
-
-		// Set up the login form.
+		setContentView(R.layout.register);
+		//Password confirmation
+		String mPasswordConfirmed;
+		
+		// Set up the register form.
 		mUsername = getIntent().getStringExtra(EXTRA_EMAIL);
-		mUsernameView = (EditText) findViewById(R.id.username);
+		mUsernameView = (EditText) findViewById(R.id.register_username);
 		mUsernameView.setText(mUsername);
 
-		mPasswordView = (EditText) findViewById(R.id.password);
+		mPasswordView = (EditText) findViewById(R.id.register_password);
 		mPasswordView
 				.setOnEditorActionListener(new TextView.OnEditorActionListener() {
 					@Override
 					public boolean onEditorAction(TextView textView, int id,
 							KeyEvent keyEvent) {
-						if (id == R.id.login || id == EditorInfo.IME_NULL) {
-							attemptLogin();
+						if (id == R.id.register || id == EditorInfo.IME_NULL) {
+							attemptregister();
 							return true;
 						}
 						return false;
 					}
 				});
+		EditText mPasswordConfirmView=(EditText) findViewById(R.id.confirm_password);
+		mPasswordConfirmView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+			@Override
+			public boolean onEditorAction(TextView textView, int id,
+					KeyEvent keyEvent) {
+				if (id == R.id.register || id == EditorInfo.IME_NULL) {
+					attemptregister();
+					return true;
+				}
+				return false;
+			}
+		});
+		mregisterFormView = findViewById(R.id.register_form);
+		mregisterStatusView = findViewById(R.id.register_status);
+		mregisterStatusMessageView = (TextView) findViewById(R.id.register_status_message);
 
-		mLoginFormView = findViewById(R.id.login_form);
-		mLoginStatusView = findViewById(R.id.login_status);
-		mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
-
-		findViewById(R.id.sign_in_button).setOnClickListener(
+		findViewById(R.id.create_new_user_btn).setOnClickListener(
 				new View.OnClickListener() {
 					@Override
 					public void onClick(View view) {
-						attemptLogin();
-					}
-				});
-		findViewById(R.id.register_button).setOnClickListener(
-				new View.OnClickListener() {
-					
-					@Override
-					public void onClick(View v) {
-						createUser();
-						
+						attemptregister();
 					}
 				});
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		super.onCreateOptionsMenu(menu);
-		getMenuInflater().inflate(R.menu.login, menu);
-		return true;
-	}
-
 	/**
-	 * Attempts to sign in or register the account specified by the login form.
+	 * Attempts to sign in or register the account specified by the register form.
 	 * If there are form errors (invalid email, missing fields, etc.), the
-	 * errors are presented and no actual login attempt is made.
+	 * errors are presented and no actual register attempt is made.
 	 */
-	public void attemptLogin() {
+	public void attemptregister() {
 		if (mAuthTask != null) {
 			return;
 		}
@@ -123,23 +110,9 @@ public class Login extends Activity {
 		mUsernameView.setError(null);
 		mPasswordView.setError(null);
 
-		// Store values at the time of the login attempt.
+		// Store values at the time of the register attempt.
 		mUsername = mUsernameView.getText().toString();
 		mPassword = mPasswordView.getText().toString();
-		try {
-			PasswordCrypto encryptor = new PasswordCrypto();
-			String encryptedmPassword = encryptor.encrypt(mPassword);
-			mPassword=encryptedmPassword;
-			
-		}
-		catch(Exception e) {
-			Context context=getApplicationContext();
-			CharSequence _error= "Initial Password encryption failed!";
-			int duration=Toast.LENGTH_LONG;
-			Toast toast= Toast.makeText(context, _error, duration);
-			toast.show();
-			
-		}
 
 		boolean cancel = false;
 		View focusView = null;
@@ -154,42 +127,23 @@ public class Login extends Activity {
 			focusView = mPasswordView;
 			cancel = true;
 		}
-		
+
 		if (cancel) {
-			// There was an error; don't attempt login and focus the first
+			// There was an error; don't attempt register and focus the first
 			// form field with an error.
 			focusView.requestFocus();
 		} else {
 			// Show a progress spinner, and kick off a background task to
-			// perform the user login attempt.
-			//Checking for email and password
-			this.dh = new DatabaseHelper(this);
-			List<String> names = this.dh.selectAll(mUsername, mPassword);
-			mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
+			// perform the user register attempt.
+			mregisterStatusMessageView.setText(R.string.register_progress_creating_new_user);
 			showProgress(true);
-			mAuthTask = new UserLoginTask();
+			mAuthTask = new UserregisterTask();
 			mAuthTask.execute((Void) null);
-			startActivity(new Intent(this, MainActivity.class));
-			/*if(names.size() > 0 ) { 
-				//Login Successful
-				mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
-				showProgress(true);
-				mAuthTask = new UserLoginTask();
-				mAuthTask.execute((Void) null);
-				startActivity(new Intent(this, MainActivity.class));
-			}
-			else
-			{
-				mUsernameView.setError(getString(R.string.error_invalid_email));
-				mPasswordView.setError(getString(R.string.error_incorrect_password));
-			}*/
 		}
 	}
-	public void createUser() {
-		startActivity(new Intent(this, Register.class));
-	}
+
 	/**
-	 * Shows the progress UI and hides the login form.
+	 * Shows the progress UI and hides the register form.
 	 */
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
 	private void showProgress(final boolean show) {
@@ -200,40 +154,40 @@ public class Login extends Activity {
 			int shortAnimTime = getResources().getInteger(
 					android.R.integer.config_shortAnimTime);
 
-			mLoginStatusView.setVisibility(View.VISIBLE);
-			mLoginStatusView.animate().setDuration(shortAnimTime)
+			mregisterStatusView.setVisibility(View.VISIBLE);
+			mregisterStatusView.animate().setDuration(shortAnimTime)
 					.alpha(show ? 1 : 0)
 					.setListener(new AnimatorListenerAdapter() {
 						@Override
 						public void onAnimationEnd(Animator animation) {
-							mLoginStatusView.setVisibility(show ? View.VISIBLE
+							mregisterStatusView.setVisibility(show ? View.VISIBLE
 									: View.GONE);
 						}
 					});
 
-			mLoginFormView.setVisibility(View.VISIBLE);
-			mLoginFormView.animate().setDuration(shortAnimTime)
+			mregisterFormView.setVisibility(View.VISIBLE);
+			mregisterFormView.animate().setDuration(shortAnimTime)
 					.alpha(show ? 0 : 1)
 					.setListener(new AnimatorListenerAdapter() {
 						@Override
 						public void onAnimationEnd(Animator animation) {
-							mLoginFormView.setVisibility(show ? View.GONE
+							mregisterFormView.setVisibility(show ? View.GONE
 									: View.VISIBLE);
 						}
 					});
 		} else {
 			// The ViewPropertyAnimator APIs are not available, so simply show
 			// and hide the relevant UI components.
-			mLoginStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
-			mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+			mregisterStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
+			mregisterFormView.setVisibility(show ? View.GONE : View.VISIBLE);
 		}
 	}
 
 	/**
-	 * Represents an asynchronous login/registration task used to authenticate
+	 * Represents an asynchronous register/registration task used to authenticate
 	 * the user.
 	 */
-	public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+	public class UserregisterTask extends AsyncTask<Void, Void, Boolean> {
 		@Override
 		protected Boolean doInBackground(Void... params) {
 			// TODO: attempt authentication against a network service.
