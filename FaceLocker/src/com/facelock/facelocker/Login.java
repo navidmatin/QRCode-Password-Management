99ec9ece87f7159fb4bf06a26786c809a3488300
navidmatin <seyedmatin.1@osu.edu>
2014-04-07 19:51:17 -0400

    ADDING STUFF AGAIN

---
+++ FaceLocker/src/com/facelock/facelocker/Login.java
@@ -0,0 +1,280 @@
+package com.facelock.facelocker;
+
+
+import java.util.List;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Activity which displays a login screen to the user, offering registration as
+ * well.
+ */
+public class Login extends Activity {
+	/**
+	 * A dummy authentication store containing known user names and passwords.
+	 * TODO: remove after connecting to a real authentication system.
+	 */
+	private static final String[] DUMMY_CREDENTIALS = new String[] {
+			"foo@example.com:hello", "bar@example.com:world" };
+
+	/**
+	 * The default email to populate the email field with.
+	 */
+	public static final String EXTRA_EMAIL = "com.example.android.authenticatordemo.extra.EMAIL";
+	
+	/**
+	 * Keep track of the login task to ensure we can cancel it if requested.
+	 */
+	private DatabaseHelper dh;
+	private UserLoginTask mAuthTask = null;
+
+	// Values for email and password at the time of the login attempt.
+	private String mUsername;
+	private String mPassword;
+
+	// UI references.
+	private EditText mUsernameView;
+	private EditText mPasswordView;
+	private View mLoginFormView;
+	private View mLoginStatusView;
+	private TextView mLoginStatusMessageView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.login);
+
+		// Set up the login form.
+		mUsername = getIntent().getStringExtra(EXTRA_EMAIL);
+		mUsernameView = (EditText) findViewById(R.id.username);
+		mUsernameView.setText(mUsername);
+
+		mPasswordView = (EditText) findViewById(R.id.password);
+		mPasswordView
+				.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+					@Override
+					public boolean onEditorAction(TextView textView, int id,
+							KeyEvent keyEvent) {
+						if (id == R.id.login || id == EditorInfo.IME_NULL) {
+							attemptLogin();
+							return true;
+						}
+						return false;
+					}
+				});
+
+		mLoginFormView = findViewById(R.id.login_form);
+		mLoginStatusView = findViewById(R.id.login_status);
+		mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
+
+		findViewById(R.id.sign_in_button).setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						attemptLogin();
+					}
+				});
+		findViewById(R.id.register_button).setOnClickListener(
+				new View.OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						createUser();
+						
+					}
+				});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+
+	/**
+	 * Attempts to sign in or register the account specified by the login form.
+	 * If there are form errors (invalid email, missing fields, etc.), the
+	 * errors are presented and no actual login attempt is made.
+	 */
+	public void attemptLogin() {
+		if (mAuthTask != null) {
+			return;
+		}
+
+		// Reset errors.
+		mUsernameView.setError(null);
+		mPasswordView.setError(null);
+
+		// Store values at the time of the login attempt.
+		mUsername = mUsernameView.getText().toString();
+		mPassword = mPasswordView.getText().toString();
+		try {
+			PasswordCrypto encryptor = new PasswordCrypto();
+			String encryptedmPassword = encryptor.encrypt(mPassword);
+			mPassword=encryptedmPassword;
+			
+		}
+		catch(Exception e) {
+			Context context=getApplicationContext();
+			CharSequence _error= "Initial Password encryption failed!";
+			int duration=Toast.LENGTH_LONG;
+			Toast toast= Toast.makeText(context, _error, duration);
+			toast.show();
+			
+		}
+
+		boolean cancel = false;
+		View focusView = null;
+
+		// Check for a valid password.
+		if (TextUtils.isEmpty(mPassword)) {
+			mPasswordView.setError(getString(R.string.error_field_required));
+			focusView = mPasswordView;
+			cancel = true;
+		} else if (mPassword.length() < 4) {
+			mPasswordView.setError(getString(R.string.error_invalid_password));
+			focusView = mPasswordView;
+			cancel = true;
+		}
+		
+		if (cancel) {
+			// There was an error; don't attempt login and focus the first
+			// form field with an error.
+			focusView.requestFocus();
+		} else {
+			// Show a progress spinner, and kick off a background task to
+			// perform the user login attempt.
+			//Checking for email and password
+			this.dh = new DatabaseHelper(this);
+			List<String> names = this.dh.selectAll(mUsername, mPassword);
+			mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
+			showProgress(true);
+			mAuthTask = new UserLoginTask();
+			mAuthTask.execute((Void) null);
+			startActivity(new Intent(this, MainActivity.class));
+			/*if(names.size() > 0 ) { 
+				//Login Successful
+				mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
+				showProgress(true);
+				mAuthTask = new UserLoginTask();
+				mAuthTask.execute((Void) null);
+				startActivity(new Intent(this, MainActivity.class));
+			}
+			else
+			{
+				mUsernameView.setError(getString(R.string.error_invalid_email));
+				mPasswordView.setError(getString(R.string.error_incorrect_password));
+			}*/
+		}
+	}
+	public void createUser() {
+		startActivity(new Intent(this, Register.class));
+	}
+	/**
+	 * Shows the progress UI and hides the login form.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+	private void showProgress(final boolean show) {
+		// On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+		// for very easy animations. If available, use these APIs to fade-in
+		// the progress spinner.
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+			int shortAnimTime = getResources().getInteger(
+					android.R.integer.config_shortAnimTime);
+
+			mLoginStatusView.setVisibility(View.VISIBLE);
+			mLoginStatusView.animate().setDuration(shortAnimTime)
+					.alpha(show ? 1 : 0)
+					.setListener(new AnimatorListenerAdapter() {
+						@Override
+						public void onAnimationEnd(Animator animation) {
+							mLoginStatusView.setVisibility(show ? View.VISIBLE
+									: View.GONE);
+						}
+					});
+
+			mLoginFormView.setVisibility(View.VISIBLE);
+			mLoginFormView.animate().setDuration(shortAnimTime)
+					.alpha(show ? 0 : 1)
+					.setListener(new AnimatorListenerAdapter() {
+						@Override
+						public void onAnimationEnd(Animator animation) {
+							mLoginFormView.setVisibility(show ? View.GONE
+									: View.VISIBLE);
+						}
+					});
+		} else {
+			// The ViewPropertyAnimator APIs are not available, so simply show
+			// and hide the relevant UI components.
+			mLoginStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
+			mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+		}
+	}
+
+	/**
+	 * Represents an asynchronous login/registration task used to authenticate
+	 * the user.
+	 */
+	public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+		@Override
+		protected Boolean doInBackground(Void... params) {
+			// TODO: attempt authentication against a network service.
+
+			try {
+				// Simulate network access.
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				return false;
+			}
+
+			for (String credential : DUMMY_CREDENTIALS) {
+				String[] pieces = credential.split(":");
+				if (pieces[0].equals(mUsername)) {
+					// Account exists, return true if the password matches.
+					return pieces[1].equals(mPassword);
+				}
+			}
+
+			// TODO: register the new account here.
+			return true;
+		}
+
+		@Override
+		protected void onPostExecute(final Boolean success) {
+			mAuthTask = null;
+			showProgress(false);
+
+			if (success) {
+				finish();
+			} else {
+				mPasswordView
+						.setError(getString(R.string.error_incorrect_password));
+				mPasswordView.requestFocus();
+			}
+		}
+
+		@Override
+		protected void onCancelled() {
+			mAuthTask = null;
+			showProgress(false);
+		}
+	}
+}
---
+++ FaceLocker/src/com/facelock/facelocker/PasswordManager.java
@@ -0,0 +1,143 @@
+package com.facelock.facelocker;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import android.content.SharedPreferences;
+import android.util.Log;
+
+/**
+ * Singleton class that manages login information. 
+ * Login information is mapped to an application for sorting and searching by the user.
+ * Multiple passwords can be stored for a single application.
+ * 
+ * @Version 1.0
+ * @author Hector Medina-Fetterman
+ */
+public class PasswordManager {
+	private static PasswordManager instance = new PasswordManager();
+	private HashMap<String, HashMap<String, LoginInformation>> passwordMap;
+	private PasswordCrypto encryptor;
+	
+	
+	private PasswordManager(){
+		passwordMap = new HashMap<String, HashMap<String, LoginInformation>>();
+		try {
+			encryptor = new PasswordCrypto();
+		} catch (Exception e) {
+			
+		}
+	}
+	
+	/**
+	 * @return The singleton instance of the PasswordManager class
+	 */
+	public static PasswordManager getInstance(){
+		return instance;
+	}
+	
+	private static String getApplicationUsernameKey (String application, String username) {
+		return application + "#" + username;
+	}
+	
+	/**
+	 * Stores login information in the password manager for later retrieval and storage.
+	 * Information is encrypted on storage.
+	 * 
+	 * @param application The application associated with the login information
+	 * @param username The username used to login to the application
+	 * @param password The unencrypted password used to login to the application
+	 */
+
+	public Boolean storeLogin(String application, String username, String password, SharedPreferences logins) {
+		String key = getApplicationUsernameKey(application, username);
+		SharedPreferences.Editor editor = logins.edit();
+		String encryptedPassword;
+		try {
+			encryptedPassword = encryptor.encrypt(password);
+
+		} catch (Exception e) {
+			return false;
+		}
+		
+		editor.putString(key, encryptedPassword);
+    	return editor.commit();
+	}
+	
+	
+	public void getLogins(SharedPreferences logins) {
+		Map<String, ?> pairs = logins.getAll();
+    	Set<String> keys = pairs.keySet();
+    	
+    	for (String key : keys) {
+    		String[] appUserCombo = key.split("#", 2);
+    		HashMap<String, LoginInformation> applicationMap = passwordMap.get(appUserCombo[0]);
+    		if(applicationMap == null)
+    			applicationMap = new HashMap<String, LoginInformation>();
+    		applicationMap.put(appUserCombo[1], new LoginInformation(appUserCombo[0], appUserCombo[1], pairs.get(key).toString()));
+    		passwordMap.put(appUserCombo[0], applicationMap);
+    	}
+	}
+	
+	public String getPassword(String application, String username, SharedPreferences logins) {
+		String applicationUsernameKey = getApplicationUsernameKey(application, username);
+		String encryptedPassword = logins.getString(applicationUsernameKey, "");
+		String decryptedPassword;
+		try {
+			decryptedPassword = encryptor.decrypt(encryptedPassword);
+
+		} catch (Exception e) {
+			decryptedPassword = e.toString();
+		}
+		return decryptedPassword;
+	}
+	
+	public static Boolean deleteAll (SharedPreferences logins) {
+		SharedPreferences.Editor editor = logins.edit();
+		editor.clear();
+		instance = new PasswordManager();
+    	return editor.commit();
+	}
+	
+	
+	/**
+	 * Retrieves the login information for a certain application and username.
+	 * 
+	 * @param application The application name
+	 * @param username The username for the given login information
+	 * @return The login information associated with the application and username, or null if no information exists
+	 *//*
+	public LoginInformation getLogin(String application, String username){
+		HashMap<String, LoginInformation> applicationMap = passwordMap.get(application);
+		if(applicationMap == null)
+			return null;
+		LoginInformation val = applicationMap.get(username);
+		return val;
+	}
+	
+	*//**
+	 * Retrieves a HashMap of usernames to login information for the given application name.
+	 * 
+	 * @param application The application name
+	 * @return A mapping of usernames to login information for a given application
+	 *//*
+	public HashMap<String, LoginInformation> getLogins(String application){
+		return new HashMap<String,LoginInformation>(passwordMap.get(application));
+	}*/
+	
+	public HashMap<String, List<String>> getMap(){
+		HashMap<String, List<String>> val = new HashMap<String, List<String>>();
+		for(String key : passwordMap.keySet()){
+			Set<String> temp = passwordMap.get(key).keySet();
+			val.put(key, Arrays.asList(temp.toArray(new String[temp.size()])));
+		}
+		return val;
+	}
+	
+	public String[] getApplications(){
+		return passwordMap.keySet().toArray(new String[passwordMap.size()]);
+	}
+}
